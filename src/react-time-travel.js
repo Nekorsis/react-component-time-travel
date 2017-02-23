a34e26a4676e903c6e1b0f563ace802363baee5e
Maxim Samoilov <samoilowmaxim@gmail.com>
2017-02-23 08:07:07 +0300

    add navigation

--- src/react-time-travel.js
+++ src/react-time-travel.js
@@ -5,27 +5,43 @@ export function timeTravel(ComponentClass) {
     constructor() {
       super()
       this.initHistory = ({ state, props }) => {
-        if (this.history) return
-        this.history = [{ state, props }]
+        this.setState({ history: [{ state, props }] })
       }
 
       this.revision = ({ props, statePatch }) => {
-        const prevRecord = last(this.history)
-        const newRecord = {
-          props: props
-            ? props
-            : prevRecord.props,
-          state: statePatch
-            ? Object.assign({}, prevRecord.state, statePatch)
-            : prevRecord.state
-        }
-        this.history.push(newRecord)
+        this.setState(state => {
+          const prevRecord = last(state.history)
+          const newRecord = {
+            props: props
+              ? props
+              : prevRecord.props,
+            state: statePatch
+              ? Object.assign({}, prevRecord.state, statePatch)
+              : prevRecord.state
+          }
+
+          const revisionIndex = this.getIndexAsNumber(state.currentRevisionIndex) + 1
+          return {
+            currentRevisionIndex: revisionIndex,
+            history: [...state.history.slice(0, revisionIndex), newRecord]
+          }
+        })
       }
-      window.___getHistory = () => this.history
+
+      this.state = {
+        currentRevisionIndex: 'last', // number | 'last'
+      }
     }
 
-    componentWillUpdate(nextProps) {
-      this.revision({ props: nextProps })
+    componentWillUpdate(nextProps, nextState) {
+      console.log('nextState.history', nextState.history)
+      if (this.props !== nextProps) {
+        this.revision({ props: nextProps })
+      }
+      if (this.state.currentRevisionIndex !== nextState.currentRevisionIndex) {
+        const statePatch = this.getCurrentRevision(nextState).state
+        this.node.__originalSetState(statePatch)
+      }
     }
 
     render() {
@@ -47,18 +63,65 @@ export function timeTravel(ComponentClass) {
       }
 
       const ref = node => {
-        if (!node) return
-
+        if (!node || this.node) return
         this.initHistory({ props: this.props, state: node.state })
-        const originalSetState = node.setState.bind(node)
-        if (!node.isPatchedByReactTimeTravel) {
-          node.setState = patchSetState(originalSetState)
-          node.isPatchedByReactTimeTravel = true
-        }
+        node.__originalSetState = node.setState.bind(node)
+        node.setState = patchSetState(node.__originalSetState)
+        this.node = node
       }
 
-      const props = Object.assign({ ref }, this.props)
-      return React.createElement(ComponentClass, props)
+      const normilizeRevisionIndex = index => {
+        return this.isLastRevisionIndex(index) ? 'last' : index
+      }
+
+      const updateRevisionIndex = update => {
+        this.setState(state => {
+          return {
+            currentRevisionIndex: update(this.getIndexAsNumber(state.currentRevisionIndex)),
+          }
+        })
+      }
+
+      const setRevisionIndex = index => {
+        updateRevisionIndex(() => index)
+      }
+
+      const navigation = this.state.history && React.DOM.div(
+        { style: { position: 'absolute' } },
+        React.DOM.button({
+          onClick: () => setRevisionIndex(0),
+          disabled: this.getIndexAsNumber(this.state.currentRevisionIndex) === 0,
+        }, '⇤'),
+        React.DOM.button({
+          onClick: () => updateRevisionIndex(x => x - 1),
+          disabled: this.getIndexAsNumber(this.state.currentRevisionIndex) === 0,
+        }, '←'),
+        React.DOM.button({
+          onClick: () => updateRevisionIndex(x => x + 1),
+          disabled: this.getIndexAsNumber(this.state.currentRevisionIndex) === this.state.history.length - 1,
+        }, '→'),
+        React.DOM.button({
+          onClick: () => setRevisionIndex('last'),
+          disabled: this.getIndexAsNumber(this.state.currentRevisionIndex) === this.state.history.length - 1,
+        }, '⇥'),
+      )
+
+      const props = Object.assign({ ref }, this.state.history ? this.getCurrentRevision(this.state).props : this.props)
+      return React.DOM.span(
+        null,
+        navigation,
+        React.createElement(ComponentClass, props)
+      )
+    }
+
+    getIndexAsNumber(index) {
+      return index === 'last'
+        ? this.state.history.length - 1
+        : index
+    }
+
+    getCurrentRevision(state) {
+      return state.history[this.getIndexAsNumber(state.currentRevisionIndex)]
     }
   }
 }
